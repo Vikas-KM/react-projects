83604d501d339b3d4bb3d81a15dda5e191bb730b
Vikas KM <vikki.km@gmail.com>
2019-01-01 12:23:44 +0530

    Updates

---
+++ precache-manifest.6df64cc079ade3955f49cad062809b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "719a7aa7c6c1b02d654a",
+    "url": "/react-projects/static/css/main.a797a13f.chunk.css"
+  },
+  {
+    "revision": "719a7aa7c6c1b02d654a",
+    "url": "/react-projects/static/js/main.719a7aa7.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-projects/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "99a6936dea147321fcfd",
+    "url": "/react-projects/static/js/runtime~main.99a6936d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-projects/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "39f1053fcfbcf640d1bf06b6e06876d0",
+    "url": "/react-projects/index.html"
+  }
+];
