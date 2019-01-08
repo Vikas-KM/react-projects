16040a80a8be142b6e89350defe73d367c948c76
Vikas KM <vikki.km@gmail.com>
2019-01-08 21:07:07 +0530

    added state-booklist

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -7,8 +7,13 @@ const ProjectList = () => {
       id: 1,
       name: "Persons List",
       url: "https://vikas-km.github.io/persons-list/",
-      desc:
-        "Explains the prop passing from one component to the other component."
+      desc: "Passing data from one component to the other component."
+    },
+    {
+      id: 2,
+      name: "State BookList",
+      url: "https://vikas-km.github.io/state-booklist/",
+      desc: "Method passing, Prop drilling and State usage."
     }
   ];
 
